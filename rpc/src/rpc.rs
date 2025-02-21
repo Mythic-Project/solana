590132b0dd399ac410628d4651210804682d2b13
GroovieGermanikus <groovie@mango.markets>
2025-02-21 08:17:57 +0100

    patch: do not apply ccount-index-include-key whitelisted pubkey on token owner+mints

--- rpc/src/rpc.rs
+++ rpc/src/rpc.rs
@@ -2294,11 +2294,6 @@ impl JsonRpcRequestProcessor {
             .account_indexes
             .contains(&AccountIndex::SplTokenOwner)
         {
-            if !self.config.account_indexes.include_key(&owner_key) {
-                return Err(RpcCustomError::KeyExcludedFromSecondaryIndex {
-                    index_key: owner_key.to_string(),
-                });
-            }
             self.get_filtered_indexed_accounts(
                 &bank,
                 &IndexKey::SplTokenOwner(owner_key),
@@ -2342,11 +2337,6 @@ impl JsonRpcRequestProcessor {
             .account_indexes
             .contains(&AccountIndex::SplTokenMint)
         {
-            if !self.config.account_indexes.include_key(&mint_key) {
-                return Err(RpcCustomError::KeyExcludedFromSecondaryIndex {
-                    index_key: mint_key.to_string(),
-                });
-            }
             self.get_filtered_indexed_accounts(
                 &bank,
                 &IndexKey::SplTokenMint(mint_key),
